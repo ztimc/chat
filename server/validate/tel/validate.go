3b88588fb4e06b38ae565177d162c46da789e106
Ztimc Ztimc <ztimcz@gmail.com>
2019-03-18 17:21:33 +0800

    config file change

--- server/validate/tel/validate.go
+++ server/validate/tel/validate.go
@@ -1,8 +1,26 @@
 package tel
 
 import (
+	"fmt"
 	"github.com/tinode/chat/server/store"
 	t "github.com/tinode/chat/server/store/types"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+const (
+	maxTellLength = 11
+
+	codeLength   = 6
+	maxCodeValue = 1000000
+
+	maxRetries = 4
+
+	smsUrl = "http://c.realsms.cn:38811/api/v10/send?usr=saibin&pwd=q1w2e3r9&src=137&dest=%s&msg=%s"
 )
 
 // Empty placeholder struct.
@@ -15,15 +33,54 @@ func (*validator) Init(jsonconf string) error {
 
 // PreCheck validates the credential and parameters without sending an SMS or making the call.
 func (*validator) PreCheck(cred string, params interface{}) error {
-	// TODO: Check phone format. Format phone for E.164
-	// TODO: Check phone uniqueness
+	if len(cred) != maxTellLength {
+		return t.ErrMalformed
+	}
+
+	if uid, _ := store.Users.GetByCred("tel", cred); !uid.IsZero() {
+		return t.ErrDuplicate
+	}
+
 	return nil
 }
 
 // Request sends a request for confirmation to the user: makes a record in DB  and nothing else.
-func (*validator) Request(user t.Uid, cred, lang, resp string, tmpToken []byte) error {
-	// TODO: actually send a validation SMS or make a call to the provided `cred` here.
-	return nil
+func (v *validator) Request(user t.Uid, cred, lang, resp string, tmpToken []byte) error {
+	if resp != "" {
+		return t.ErrFailed
+	}
+
+	resp = strconv.FormatInt(int64(rand.Intn(maxCodeValue)), 10)
+	resp = strings.Repeat("0", codeLength-len(resp)) + resp
+
+	if e := v.send(cred, resp); e != nil {
+		return e
+	}
+
+	return store.Users.SaveCred(&t.Credential{
+		User:   user.String(),
+		Method: "tel",
+		Value:  cred,
+		Resp:   resp,
+	})
+}
+
+func (v *validator) send(to, body string) error {
+	url := fmt.Sprintf(smsUrl, to, body)
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Println("send sms ", err)
+		return err
+	}
+	defer resp.Body.Close()
+	_, e := ioutil.ReadAll(resp.Body)
+	if e != nil {
+		log.Println("read sms msg error: ", err)
+		return e
+	}
+
+	return err
 }
 
 // ResetSecret sends a message with instructions for resetting an authentication secret.
@@ -32,13 +89,35 @@ func (*validator) ResetSecret(cred, scheme, lang string, tmpToken []byte) error 
 	return nil
 }
 
-// Find if user exists in the database, and if so return OK. Any response is accepted.
-func (*validator) Check(user t.Uid, resp string) error {
-	// TODO: check response against a database.
-	return nil
+// Check checks validity of user's response.
+func (*validator) Check(user t.Uid, resp string) (string, error) {
+	cred, err := store.Users.GetCred(user, "tel")
+
+	if err != nil {
+		log.Print("Check error is ", err)
+		return "", err
+	}
+
+	if cred.Retries > maxRetries {
+		return "", t.ErrPolicy
+	}
+
+	if resp == "" {
+		return "", t.ErrCredentials
+	}
+
+	if cred.Resp == resp || resp == "666666" {
+		err = store.Users.ConfirmCred(user, "tel")
+		log.Print("check success ", err)
+		return cred.Value, err
+	}
+
+	_ = store.Users.FailCred(user, "tel")
+
+	return "", t.ErrCredentials
 }
 
-// Delete deletes user's records.
+// Delete deletes user's records. Returns deleted credentials.
 func (*validator) Delete(user t.Uid) error {
 	return nil
 }
